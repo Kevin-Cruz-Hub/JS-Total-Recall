cc24fc6d0e3fa8e6895a1135de7b3ab6961b6c4d
Kevin <kevincruz042@gmail.com>
2023-07-03 13:50:07 -0400

    The Farm

---
+++ labs.js
@@ -0,0 +1,58 @@
+// Q + A
+/*
+1. To assign a value to a variable by using the (=) sign, along with either a let or const and the variable name
+2. To change the value of a variable you need to use the assignment operator (=) to equal the wanted value
+3. To Assign an existing value to an existing variable you need to use the assignment operator (=) to equal the existing variable wanted.
+4. declaring: creates a new variable within a program, assign: is when you make a variable equal a specific value, define: is to call an existing variable 
+5. Pseudocoding: is when you create a layout of the functions for your program before typing the code out
+6.
+*/
+
+// ==================================================
+// STRINGS
+// // 1.
+// let firstVariable;
+// // 2.
+// firstVariable = 'Hello World';
+// // 3.
+// firstVariable = 10;
+// // 4.
+// let secondVariable = firstVariable;
+// // 5.
+// secondVariable = 'Welcome';
+// // 6.
+// console.log(firstVariable);
+// // 7.
+// let yourName = 'Kevin';
+// let greet = 'Hello, my name is ';
+// console.log(greet + yourName);
+
+// ======================================================
+// BOOLEANS
+// const a = 4;
+// const b = 53;
+// const c = 57;
+// const d = 16;
+// const e = 'Kevin';
+
+// console.log(a < b);
+// console.log(c > d);
+// console.log('Name'==='Name');
+// // FOR THE NEXT TWO, USE ONLY && OR ||
+// console.log(true || false);
+// console.log(false || false || false || false || false || true);
+// console.log(false === false)
+// console.log(e === 'Kevin');
+// console.log(a < b + c); // note: a < b < c is NOT CORRECT (and is not a valid JS expression, think about using other math operations)
+// console.log(a > a - d); // note: the answer is a simple arithmetic equation, not something "weird"
+// console.log(48 !== '48');
+
+// =================================================================
+
+// The Farm
+let animal = 'rat';
+if(animal === 'cow'){
+    console.log('mooooo');
+}else{
+    console.log("Hey! You're now a cow." );
+}
